f3cea801e6d721de22cbac8dd7ca1eb3aa8f323e
ShivamShrirao <shivamshrirao@gmail.com>
2020-09-09 22:02:13 +0530

    files webpage template made

--- www/server.py
+++ www/server.py
@@ -3,13 +3,17 @@
 from flask import Flask, request, render_template
 import numpy as np
 import json
+from pymongo import MongoClient
+
+client = MongoClient("localhost", 27017)
+db = client.IMDB
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-	return render_template("index.html")
+	return render_template("base.html")
 
 
 @app.route('/submit', methods=['POST'])
