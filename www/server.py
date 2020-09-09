1c1eb20c754a44af5e4900b50cb4dfa9856984dd
ShivamShrirao <shivamshrirao@gmail.com>
2020-09-10 02:38:49 +0530

    search working with b

--- www/server.py
+++ www/server.py
@@ -3,6 +3,7 @@
 from flask import Flask, request, render_template
 import numpy as np
 import json
+import re
 from pymongo import MongoClient
 
 client = MongoClient("localhost", 27017)
@@ -19,8 +20,12 @@ def index():
 @app.route('/fetch')
 def submit():
 	term = request.args['term']
-	jj = [term]
-	return json.dumps(jj)
+	resp = []
+	if term:
+		res = db.movies.find({"title": re.compile(term, re.IGNORECASE)}, {"_id": 0, "title": 1}, limit=15)
+		for nm in res:
+			resp.append(nm["title"])
+	return json.dumps(resp)
 
 
 if __name__ == '__main__':
